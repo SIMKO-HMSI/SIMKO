fad771161cf942195bbc75c2a7658744f73ec731
Evan Reswara <Evanreswara@gmail.com>
2020-11-19 21:26:33 +0700

    create migration controller and model

---
+++ app/Http/Controllers/MoneyController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\money;
+use Illuminate\Http\Request;
+
+class MoneyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\money  $money
+     * @return \Illuminate\Http\Response
+     */
+    public function show(money $money)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\money  $money
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(money $money)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\money  $money
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, money $money)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\money  $money
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(money $money)
+    {
+        //
+    }
+}
--- database/migrations/2020_11_19_134831_create_expenses_table.php
+++ database/migrations/2020_11_19_134831_create_expenses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateExpensesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('expenses', function (Blueprint $table) {
             $table->id();
-            $table->string('fullname');
-            $table->string('nim');
+            $table->unsignedBigInteger('user_id');
+            $table->string('deskripsi');
+            $table->string('proker');
             $table->string('divisi');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('jumlah_pengeluaran');
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
@@ -33,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('expenses');
     }
 }
